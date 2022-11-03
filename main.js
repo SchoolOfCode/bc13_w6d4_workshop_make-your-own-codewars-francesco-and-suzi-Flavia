70664041f1129f595652f406319269c64ef32601
susannaclark <susannaclark95@gmail.com>
2022-11-03 14:03:16 +0000

    wrote description of kata

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 /* 
-👉 Write your kata here!
+👉 You are a club doorman, you want to make sure everyone who enters is over 18.
+You have the chance to automate this process.
+The test will pass an array of numbers into your function.
+Return an array with only numbers over 18 in it.
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
